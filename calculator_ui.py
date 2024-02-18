46498742afab1e6279687451cbc3027525e961c0
Thanchida <thanchida.a@ku.th>
2024-02-18 15:36:37 +0700

    finish3(edit history)

--- calculator_ui.py
+++ calculator_ui.py
@@ -12,7 +12,9 @@ class CalculatorUI(tk.Tk):
         self.title('Calculator')
         self.geometry('400x400')
         self.display = tk.Label(self, text='', bg='black', fg='yellow', font=('monospace', 30), anchor=tk.E)
-        self.history = tk.Label(self, text='', bg='lightgrey', fg='grey', font=('monospace', 15), anchor=tk.E)
+        self.history = tk.Label(self, text='history', fg='grey', font=('monospace', 15), anchor=tk.W)
+        self.history_menu = ttk.Combobox(self)
+        self.all_history = []
         self.keypad = Keypad(self, list('789456123 0.'), 3)
         self.operator = ['(', ')', '*', '/', '+', '-', '^', '=']
         self.operator_pad = Keypad(self, self.operator, 2)
@@ -26,12 +28,15 @@ class CalculatorUI(tk.Tk):
         self.keypad.bind('<Button>', self.handler)
         self.operator_pad.bind('<Button>', self.handler)
         self.math_func.bind('<<ComboboxSelected>>', self.func_handler)
+        self.history.bind('<Motion>', self.show_history)
+        self.history_menu.bind('<<ComboboxSelected>>', self.history_recall)
+        self.history_menu.bind('<Leave>', self.hide_history)
         pygame.mixer.init()
         self.sound = pygame.mixer.Sound('notification_sound.mp3')
 
         self.history.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
         self.display.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
-        self.math_func['value'] = ['exp', 'ln', 'log', 'log2', 'sqrt', 'sin', 'cos', 'tan']
+        self.math_func['value'] = ['exp', 'ln', 'log10', 'log2', 'sqrt', 'sin', 'cos', 'tan']
         self.math_func.config(width=6, height=15)
         self.math_func.pack(side=tk.TOP, anchor=tk.CENTER, padx=5, expand=True, fill=tk.BOTH)
         self.clr.config(width=3, height=2)
@@ -55,28 +60,45 @@ class CalculatorUI(tk.Tk):
         check = text[-1] if text else None
         if not text or check in self.operator:
             text += choice + '('
-            self.display.config(text=text)
+            self.display.config(text=text, fg='yellow')
         elif check in num:
             new = choice + '(' + text + ')'
-            self.display.config(text=new)
+            self.display.config(text=new, fg='yellow')
 
     def calculate(self):
+        check = True
         text = self.display['text']
         text = text.replace('sin', 'math.sin')
         text = text.replace('cos', 'math.cos')
         text = text.replace('tan', 'math.tan')
         text = text.replace('ln', 'math.log')
-        text = text.replace('log', 'math.log10')
+        text = text.replace('log', 'math.log')
         try:
             result = eval(text)
-            history = self.history['text'] + '\n' + text + '=' + str(result)
+            history = self.display['text'] + '=' + str(result)
         except Exception:
             result = "Invalid Result"
             self.display.config(text=result, fg='red')
             self.sound.play()
             return
-        self.display.config(text=result)
-        self.history.config(text=history)
+        self.display.config(text=result, fg='yellow')
+        self.all_history.append(history)
+        self.history_menu['values'] = self.all_history
+
+    def show_history(self, event):
+        self.history_menu.pack(side=tk.TOP, anchor=tk.E)
+
+    def history_recall(self, event):
+        recall = self.history_menu.get()
+        n = 0
+        for key in recall:
+            n += 1
+            if key == '=':
+                recall = recall[:n-1]
+        self.display.config(text=recall)
+
+    def hide_history(self, event):
+        self.history_menu.pack_forget()
 
     def clear(self):
         self.display.config(text='', fg='yellow')
